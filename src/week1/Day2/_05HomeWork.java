56d5dce1a9721b4e5370fbc4c4af94035e5ee5da
RUSLAN <RUSLAN>
2016-12-10 03:07:21 +0200

    second less

---
+++ src/week1/Day2/_05HomeWork.java
@@ -0,0 +1,29 @@
+package week1.Day2;
+
+import java.util.Scanner;
+
+/**
+ * Created by User on 07.12.2016.
+ */
+//5) Даны три числа a, b, c. Определить, имеется ли среди них хотя бы одна
+//пара равных
+public class _05HomeWork {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System. in);
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        boolean result = a == b;
+            System.out.println(result);
+        boolean resulta = a == c;
+            System.out.println(result);
+        boolean resultb = b == c;
+            System.out.println(result);
+
+
+
+
+
+    }
+}
